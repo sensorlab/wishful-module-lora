48f0263caaa673ab5d1c8e0d8131e181100a674d
Matevz Vucnik <vucnikm@gmail.com>
2017-10-04 17:36:00 +0200

    fix lora

--- wishful_module_lora/module_lora.py
+++ wishful_module_lora/module_lora.py
@@ -6,8 +6,6 @@ from wishful_framework.classes import exceptions
 
 import time
 from vesna import alh
-from vesna.alh.spectrumsensor import SpectrumSensor
-from vesna.alh.signalgenerator import SignalGenerator, SignalGeneratorProgram
 
 __author__ = "Matevz Vucnik"
 __copyright__ = "Copyright (c) 2017, Jozef Stefan Instiute"
@@ -15,4 +13,31 @@ __version__ = "0.1.0"
 __email__ = "matevz.vucnik@ijs.si"
 
 
-# TODO: Implement LORA UPIs
+@wishful_module.build_module
+class LoraModule(wishful_module.AgentModule):
+    node = None
+    
+    def __init__(self, service, serial):
+        super(LoraModule, self).__init__()
+        self.log = logging.getLogger('LoraModule')
+        self.node = alh.ALHWeb(service + "/communicator", serial)
+
+    @wishful_module.bind_function(upis.radio.get_radio_info)
+    def get_radio_info(self, platform_id):
+        return self.node.get("loraRadioInfo").decode('ascii').strip()
+
+    @wishful_module.bind_function(upis.radio.set_parameters)
+    def set_parameters(self, params):
+        return "hello"
+
+    @wishful_module.bind_function(upis.radio.set_tx_power)
+    def set_tx_power(self, params):
+        return "hello"
+
+    @wishful_module.bind_function(upis.radio.set_rxchannel)
+    def set_rxchannel(self, params):
+        return "hello"
+
+    @wishful_module.bind_function(upis.radio.set_txchannel)
+    def set_txchannel(self, params):
+        return "hello"
